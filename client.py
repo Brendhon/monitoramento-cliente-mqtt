ad9e8897c9b03161a091933096f5c2e30732f6ee
Brendhon <brendhon.e.c.m@gmail.com>
2020-12-12 15:31:49 -0300

    create: server.py

--- client.py
+++ client.py
@@ -8,29 +8,33 @@ mqttBroker = "test.mosquitto.org"
 client = mqtt.Client("Cliente")
 client.connect(mqttBroker) 
 
+idList = [
+    "5fd2b042c9671d3d14065858",
+    "5fd2b042c9671d3d14065859",
+    "5fd2b042c9671d3d1406585a",
+    "5fd2b043c9671d3d1406585b",
+    "5fd2b043c9671d3d1406585c",
+    "5fd2b043c9671d3d1406585d",
+    "5fd2b043c9671d3d1406585e",
+    "5fd2b043c9671d3d1406585f",
+    "5fd2b043c9671d3d14065860",
+    "5fd2b043c9671d3d14065861",
+    "5fd2b044c9671d3d14065862",
+    "5fd2b044c9671d3d14065863"
+]
+
 while True:
 
     # Alterando os valores
     randId = randrange(12)
     randStatus = randint(0, 1)
-    randType = randint(1, 3)
-
-    if randType == 1:
-        randType = "NORMAL"
-    
-    if randType == 2:
-        randType = "DEFICIENTE"
-
-    if randType == 3:
-        randType = "IDOSO"
 
     obj = {
-        'id': randId,
-        'status': randStatus,
-        'type': randType
+        '_id': idList[randId],
+        'status': randStatus
     }
 
     # Enviando para o tópico 
     client.publish("c115/projeto/vaga", json.dumps(obj))
     print("Mudança na vaga: " + str(randId)) # Mostrando no console qual vaga foi alterada
-    time.sleep(2) # Tempo para alterar
+    time.sleep(10) # Tempo para alterar
